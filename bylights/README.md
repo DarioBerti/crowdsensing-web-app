901ec0d5e72948104108a00c58c3249e954c5852
DarioBerti <dario.berti@studio.unibo.it>
2024-03-07 20:22:42 +0100

    Create project without routing

--- bylights/README.md
+++ bylights/README.md
@@ -1,4 +1,4 @@
-# hello-world
+# bylights
 
 ## Project setup
 ```
--- hello-world/src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-
-/*app gets rendered @ the id=app */
-createApp(App).use(router).mount('#app')
