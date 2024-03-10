a61cdebc48ed66d2d177295d55b327525cadfd74
DarioBerti <dario.berti@studio.unibo.it>
2024-03-10 16:08:19 +0100

    [VUE] create button in App.vue

--- bylights/src/App.vue
+++ bylights/src/App.vue
@@ -1,18 +1,31 @@
 <template>
-  <div></div>
+  <div>
+    <h1>{{ message }}</h1>
+    <button @click="saluta">Cliccami</button>
+  </div>
+  <div v-if="true"></div> <!--se evento non cambia spesso-->
+  <div v-show="true"></div> <!--se evento cambia spesso-->
 </template>
 
 <script>
-  export default {
-    name: 'App'
+export default {
+  name: 'App',
+  data() {
+    return {
+      message: 'Ciao Mondo!'
+    };
+  },
+  methods: {
+    saluta() {
+      this.message = 'bye bye'
+    }
   }
+};
 </script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
