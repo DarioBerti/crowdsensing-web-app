9070037ac766b0c6bd5945e9c457717bf885d296
DarioBerti <dario.berti@studio.unibo.it>
2024-03-10 17:39:20 +0100

    [vue] Update App.vue

--- bylights/src/App.vue
+++ bylights/src/App.vue
@@ -1,10 +1,17 @@
 <template>
   <div>
     <h1>{{ message }}</h1>
-    <button @click="saluta">Cliccami</button>
+    <button @click="changeMessage">Cliccami</button>
   </div>
-  <div v-if="true"></div> <!--se evento non cambia spesso-->
-  <div v-show="true"></div> <!--se evento cambia spesso-->
+  <!--div v-if="true"></div> se evento non cambia spesso
+  <div v-show="true"></div> se evento cambia spesso
+  <div class="box" @mouseover="handleMouseOver">box</div> mouse events
+  <div>
+    <ul>  data un array, c'è metodo .filter()
+      <li v-for="book in books" :class="{ fav: book.isFav}">  ciclare una lista books ed aggiungere una classe 'fav' ad ogni classe book che ha metodo isFav==true
+      </li>
+    </ul>
+  </div-->
 </template>
 
 <script>
@@ -16,9 +23,15 @@ export default {
     };
   },
   methods: {
-    saluta() {
+    changeMessage() {
       this.message = 'bye bye'
+    },
+    handleMouseOver(e){
+      console.log(e, e.type);
     }
+  },
+  computed: {
+    //per liste che cambiano sempre in base a cosa viene cliccato(es lista dei preferiti)
   }
 };
 </script>
