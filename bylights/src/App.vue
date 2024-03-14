38696a6335ddb109bca687ff1c835bb8cc2ad7ad
DarioBerti <dario.berti@studio.unibo.it>
2024-03-14 15:48:45 +0100

    Create SignunForm.vue

--- bylights/src/App.vue
+++ bylights/src/App.vue
@@ -1,45 +1,14 @@
 <template>
-  <div>
-    <h1>{{ message }}</h1>
-    <input type="text" ref="inputText">
-    <button @click="handleClickButton">button</button>
-  </div>
-  <div v-if="showModal">
-    <!--@close è emit del componente. genitore invia dati, figlio risponde ad eventi attraverso emit, senza modificare-->
-    <ModalComponent :header="header" :theme="theme" :showModal="showModal" @close="toggleModal"/>
-  </div>
-  <button @click="toggleModal">open modal</button>
+  <SignupForm />
 </template>
 
 <script>
-  import ModalComponent from './components/ModalComponent.vue'
+  import SignupForm from './components/SignupForm.vue'
 
   export default {
     name: 'App',
-    components: {ModalComponent},
-    data() {
-      return {
-        message: 'My first vue web-app',
-        header: 'sign up with account',
-        theme: 'sale',
-        showModal: false
-      };
-    },
-    methods: {
-      changeMessage() {
-        this.message = 'bye bye'
-      },
-      handleClickButton(){
-        this.$refs.inputText.classList.add('active') //aggiunge classe active a tale ref specifica
-      },
-      toggleModal(){
-        this.showModal = !this.showModal
-      }
-    },
-    computed: {
-      //per liste che cambiano sempre in base a cosa viene cliccato(es lista dei preferiti)
-    }
-  };
+    components: { SignupForm }
+  }
 </script>
 
 <style>
@@ -48,5 +17,10 @@
     text-align: center;
     color: #2c3e50;
     margin-top: 60px;
+  }
+
+  body{
+    margin: 0%;
+    background: #eee;
   }
 </style>
--- bylights/src/main.js
+++ bylights/src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-/*import global css file */
-import './assets/global.css'
+/*import global css file 
+import './assets/global.css'*/
 
 /*app gets rendered @ id=app */
 createApp(App).mount('#app')
