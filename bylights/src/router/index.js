fec8630f6682fd5070a9c1083959c32e09bc96ef
DarioBerti <dario.berti@studio.unibo.it>
2024-03-19 12:20:17 +0100

    create router folder, RegisterFormView, SignupFormView

--- bylights/src/router/index.js
+++ bylights/src/router/index.js
@@ -1,20 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '.. /views/Home. vue'
+import RegisterFormView from '../views/RegisterFormView.vue'
+import SignupFormView from '../views/SignupFormView.vue'
 
 const routes = [
     {
         path: '/',
-        name: 'Home',
-        component: Home
+        name: 'SignupFormView',
+        component: SignupFormView
     },
     {
-        path: '/about',
-        name: 'About',
-        // route level code-splitting
-        // this generates a separate chunk (about. [hash] -js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ ' ../views/About. vue')
-    }
+        path: '/RegisterFormView',
+        name: 'RegisterFormView',
+        component: RegisterFormView
+    },
 ]
 
 const router = createRouter({
---
+++ bylights/src/views/SignupFormView.vue
@@ -0,0 +1,103 @@
+<template>
+    <div>
+        <!--calls function 'handleSubmit' and does not reload the page-->
+        <form @submit.prevent="handleSubmit">
+            <section>
+                <h1>SIGN UP</h1>
+            </section>
+            <section>
+                <label>Email:</label>
+                <!--v-model tracks user input real-time-->
+                <input type="email" required v-model="email">
+                <label>Password</label>
+                <input type="password" required v-model="password">
+                <div v-if="this.passwordError" class="error">
+                    {{ passwordError }}
+                </div>
+                <div>
+                    <p>Don't have an account?  </p>
+                    <router-link :to="{ name: 'RegisterFormView'}"><p class="RegisterLink">Register</p></router-link>
+                </div>
+            </section>
+            <div class="submit">
+                <button>Sign up</button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'SignupFormView',
+    components: {},
+    data() {
+        return{
+            email: '',
+            password: '',
+            passwordError: ''
+        }
+    },
+    methods: {
+        handleSubmit(){
+            this.passwordError =  this.password.length < 6 ? 'the password must be at least 6 chars long!' : ''  
+        }
+    }
+}
+</script>
+
+<style scoped>
+    form {
+    max-width: 420px;
+    margin: 30px auto;
+    background:white;
+    text-align: left;
+    padding: 40px;
+    border-radius: 10px;
+    }
+    label, p{
+        color: #aaa;
+        display: inline-block;
+        margin: 25px 0 15px;
+        font-size: 0.6em;
+        text-transform: uppercase;
+        letter-spacing: 1px;
+        font-weight: bold;
+    }
+    input{
+        display: block;
+        padding: 10px 6px;
+        width: 100%;
+        box-sizing: border-box;
+        border: none;
+        border-bottom: 1px solid #ddd;
+        color: #555;
+    }
+    .RegisterLink{
+        color: red;
+        margin-left: 3%;
+    }
+    .RegisterLink:hover{
+         text-decoration: underline;
+         text-decoration-color: #ff0062;
+    }
+    h1{
+        color: #555;
+    }
+    button {
+        background: #0b6dff;
+        border: 0;
+        padding: 10px 20px;
+        margin-top: 20px;
+        color: white;
+        border-radius: 20px;
+    }
+    .submit {
+        text-align: center;
+    }
+    .error {
+        color: #ff0062;
+        margin-top: 10px;
+        font-size: 0.8em;
+        font-weight: bold;
+    }
+</style>
