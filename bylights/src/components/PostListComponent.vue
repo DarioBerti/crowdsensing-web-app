9d8a5581d5f55348aba900110d144703ac597b52
DarioBerti <dario.berti@studio.unibo.it>
2024-04-01 14:58:02 +0200

    Create component PostListComponent.vue

---
+++ bylights/src/components/PostListComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="post-list">
+        <div v-for="post in posts" :key="post.id">
+            <h2>id del post: {{ post.id }}</h2>
+            <h3>{{ post.title }} {{ post.body }}</h3>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default{
+        //così posso usare DIRETTAMENTE il dato 'posts' che è stato passato come props, senza aggiungere nessun'altra variabile
+        props: ['posts'],
+        //se voglio usarlo dentro il setup lo passo così
+        setup(props){
+            console.log(props.posts);
+        }
+    }
+</script>
+
+<style></style>
