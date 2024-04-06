2f71bd72ffda1b6d4d6341aacfeb97ce4125a84d
DarioBerti <dario.berti@studio.unibo.it>
2024-04-06 18:53:40 +0200

    Update MapView.vue
    now starts with marker in position

--- bylights/src/views/MapView.vue
+++ bylights/src/views/MapView.vue
@@ -1,11 +1,20 @@
 <template>
-    <button @click="getLocation()" >Get Location</button>
-    <p>{{ lat }},{{ lng }}</p>
-
-    <div ref="mapContainer" style="height: 400px;"></div>
+    <div ref="mapContainer" class="full-screen-map" ></div>
 </template>
 
+<style scoped>
+    .full-screen-map {
+        width: 100vw;  /* 100% della larghezza della viewport */
+        height: 100vh; /* 100% dell'altezza della viewport */
+        position: fixed; /* Opzionale: per rendere la mappa posizionata sopra tutto */
+        top: 0;
+        left: 0;
+    }
+</style>
+
 <script>
+    import 'leaflet/dist/leaflet.css';
+    import { Icon } from 'leaflet';
     import 'leaflet/dist/leaflet.css';
     import {onMounted, ref} from 'vue'
     import L from 'leaflet'
@@ -18,17 +27,30 @@
             const lng = ref(0)
             const map = ref()
             const mapContainer = ref()
+            const defaultIcon = new Icon({
+                iconUrl: require('leaflet/dist/images/marker-icon.png'), 
+                shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+            });
+
+            L.Marker.prototype.options.icon = defaultIcon;
 
             const getLocation = () => {
                 if (navigator.geolocation) {
-                    navigator.geolocation.getCurrentPosition((position) => {
+                    navigator.geolocation.watchPosition((position) => {
                         lat.value = position.coords.latitude
                         lng.value = position.coords.longitude
+                        map.value.setView([lat.value, lng.value], 16)
+
+                        L.marker([lat.value, lng.value]).addTo(map.value);
                     })
                 }
             }
 
             const openMap = () => {
+                //legge la posizione iniziale e set di latitudine, lungitudine e marker iniziale
+                getLocation();
+
+                //set della mappa
                 map.value = L.map(mapContainer.value).setView([51.505, -0.09], 13);
                 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
                     maxZoom: 19,
@@ -42,6 +64,3 @@
         }
     }
 </script>
-
-<style scoped>
-</style>
