b70536ecb0f1227b09fbad38564a63d9f8421331
DarioBerti <dario.berti@studio.unibo.it>
2024-04-10 16:30:15 +0200

    Update MapView.vue async
    first update using async functions

--- bylights/src/views/MapView.vue
+++ bylights/src/views/MapView.vue
@@ -3,18 +3,29 @@
     <div class = "mapLayout">
         <div ref="mapContainer" class="full-screen-map" ></div>
         <div class="record-icon">
-            <router-link to="/mapRecordingView">
-                <!--binding per svg-->
-                <img :src="recordIcon" alt="record icon" class = "record-style">
-            </router-link>
+                <div v-if="changeFlag">
+                    <!--binding per svg-->
+                    <img :src="recordIcon" alt="record icon" class = "record-style" @click="switchRecording">
+                </div>
+                <div v-else>
+                    <!--binding per svg-->
+                    <img :src="stopRecordIcon" alt="stop record icon" class = "stop-record-style" @click="switchRecording">
+                </div>
         </div>
-        <div class="saved-paths">
-            <!--routing link alla pagina di MapRecordingView-->
-            <router-link to="" class="SavedPathsRoutingLink">
-                saved paths
-            </router-link>
+            <div v-if="changeFlag">
+                <div class="saved-paths">
+                    <!--routing link alla pagina di MapRecordingView-->
+                    <router-link to="" class="SavedPathsRoutingLink">
+                        saved paths
+                    </router-link>
+                </div>
+            </div>
+            <div v-else>
+                <div class="saved-paths">
+                    <p class="walkingText">walking...</p>
+                </div>
+            </div>
         </div>
-    </div>
 </template>
 
 <style scoped>
@@ -41,8 +52,20 @@
         text-align: center;
     }
     .record-style{
-        height: 15%;
-        width: 15%;
+        height: 10%;
+        width: 10%;
+    }
+    .stop-record-icon{
+        position: absolute; /*interrompe flusso del documento e viene messo in sovrapposizione in base anche al z-index */
+        bottom: 10px; /* Posiziona 10px sopra il fondo del contenitore */
+        left: 50%; /* Posiziona a metà del contenitore sull'asse orizzontale */
+        transform: translateX(-50%); /* Sposta a sinistra di metà della sua larghezza per centrarlo */
+        z-index: 1000;  /*così elemento appare al di sopra degli altri elementi del contenitore */
+        text-align: center;
+    }
+    .stop-record-style{
+        height: 10%;
+        width: 10%;
     }
     .saved-paths{
         position: absolute;
@@ -66,8 +89,27 @@
         font-weight: bold;
         text-decoration: none;
     }
+    .walkingText {
+        margin: 0;
+        background-color: white;  /* Colore di sfondo */
+        border-radius: 10px;     /* Angoli arrotondati */
+        padding: 10px;           /* Padding intorno al testo */
+        text-align: center;      /* Allineamento del testo */
+        
+        /* Altre proprietà per abbellire il link */
+        color: green;            /* Colore del testo */
+
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        font-weight: bold;
+    }
     @media (max-width: 600px) {
         .record-style{
+            height: 25%;
+            width: 25%
+        }
+    }
+    @media (max-width: 600px) {
+        .stop-record-style{
             height: 25%;
             width: 25%
         }
@@ -81,6 +123,7 @@
     import {onMounted, ref} from 'vue'
     import L from 'leaflet'
     import recordIcon from '../assets/record-svg.svg'
+    import stopRecordIcon from '../assets/stop-record-svg.svg'
 
     export default{
         name: 'MapView',
@@ -94,38 +137,62 @@
                 iconUrl: require('leaflet/dist/images/marker-icon.png'), 
                 shadowUrl: require('leaflet/dist/images/marker-shadow.png')
             });
+            const changeFlag = ref(true)
 
             L.Marker.prototype.options.icon = defaultIcon;
 
-            const getLocation = () => {
-                if (navigator.geolocation) {
-                    navigator.geolocation.watchPosition((position) => {
-                        lat.value = position.coords.latitude
-                        lng.value = position.coords.longitude
-                        map.value.setView([lat.value, lng.value], 16)
+            const getLocation = async() => {
+                return new Promise((resolve, reject) => {
+                    if(navigator.geolocation) {
+                        navigator.geolocation.getCurrentPosition(position => {
+                            lat.value = position.coords.latitude
+                            lng.value = position.coords.longitude
 
-                        L.marker([lat.value, lng.value]).addTo(map.value);
-                    })
+                            resolve()
+                        }, reject)
+                    }else{
+                        reject('Geolocation is not supported in this browser')
+                    }
+                })
+            }
+
+            const openMap = async() => {
+                try{
+                    console.log("MOUNTED MAP VIEW")
+
+                    //legge la posizione iniziale e set di latitudine, lungitudine e marker iniziale
+                    await getLocation()
+
+                    //set del map.value
+                    map.value = L.map(mapContainer.value, {zoomControl: false}).setView([lat.value, lng.value], 16);
+                    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                        maxZoom: 19,
+                        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                    }).addTo(map.value);
+
+                    //aggiunta marker
+                    L.marker([lat.value, lng.value]).addTo(map.value);
+
+
+                    // Utilizza setTimeout per richiamare invalidateSize dopo che la mappa è stata caricata
+                    setTimeout(function () {
+                        map.value.invalidateSize();
+                    }, 100);
+                }catch (error){
+                    console.log('Failed to get Location:', error)
                 }
+
             }
 
-            const openMap = () => {
-                console.log("MOUNTED MAP VIEW")
-
-                //legge la posizione iniziale e set di latitudine, lungitudine e marker iniziale
-                getLocation();
-
-                //set del map.value
-                map.value = L.map(mapContainer.value, {zoomControl: false}).setView([51.505, -0.09], 13);
-                L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                    maxZoom: 19,
-                    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-                }).addTo(map.value);
+            const switchRecording = () => {
+                changeFlag.value = !changeFlag.value
             }
 
-            onMounted(openMap)
+            onMounted(() => {
+                openMap()
+            })
 
-            return{lat, lng, getLocation, map, mapContainer, openMap, recordIcon}
+            return{lat, lng, getLocation, map, mapContainer, openMap, recordIcon, changeFlag, switchRecording, stopRecordIcon}
         }
     }
 </script>
