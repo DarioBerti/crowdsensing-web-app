b1439323349c40933b9f515a216b71e1f5cc5406
DarioBerti <dario.berti@studio.unibo.it>
2024-05-03 14:49:35 +0200

    Create test1.php
    first connection frontend-backend

--- bylights/src/views/SavedPathsView.vue
+++ bylights/src/views/SavedPathsView.vue
@@ -1,9 +1,47 @@
+<script>
+export default {
+  name: 'SavedPathsView',
+  data() {
+    return {
+      info: null,
+      error: null
+    };
+  },
+  methods: {
+    fetchData() {
+      fetch('http://localhost/tirocinio/crowdsensing-web-app/bylights/db/api/test1.php')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        this.info = data;
+      })
+      .catch(error => {
+        this.error = error.message;
+      });
+    }
+  },
+  mounted() {
+    this.fetchData();
+  }
+};
+</script>
+
 <template>
-    <div>
-        saved paths view
+  <div>
+    <h1>Data from Backend</h1>
+    <div v-if="error">
+      <p>Error: {{ error }}</p>
     </div>
+    <div v-else-if="info">
+      <p>Status: {{ info.status }}</p>
+      <p>Message: {{ info.data.message }}</p>
+    </div>
+    <div v-else>
+      <p>Loading data...</p>
+    </div>
+  </div>
 </template>
-
-<script></script>
-
-<style></style>
